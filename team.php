93e2e00842429265243518cd915fbff665fc6ffd
hfilali <hadil.filali@sparkline.fr>
2021-07-05 22:25:09 +0200

    suppression anciens collaborateurs et mise à jour par nouveaux membres de l'équipe

--- team.php
+++ team.php
@@ -25,7 +25,7 @@
             <div class="span12 themeapt_animated_text themeapt_text  themeapt_animate_when_almost_visible left-to-right">
                 <!--   Big Title wrap -->	
 			    <div class="big-title">
-                    <h1>Une équipe dynamique et passionnée à votre écoute</h1>
+                    <h1>Expertises, Passion et Connexion à un réseau de spécialistes pour concrétiser votre projet</h1>
                 </div>
                 <!--   hr-Start-->			  
 			    <div class="hr hr-center"> <!--   hr-center/ hr-left/ hr-right/  hr-short/ hr-light/-->
@@ -35,15 +35,15 @@
 			    </div>
 			    <!--   hr-End-->	
                 <p class="center">
-                    Comprendre le parcours et le projet professionnels du  Candidat pour cibler le bon projet d'entreprise.
+                    Comprendre le parcours et le projet professionnels du Candidat pour cibler le bon projet d'entreprise
                 </p>					
             </div><!-- /.span12 -->		
         </div><!-- /.row -->		 
 	</div><!-- /.container -->
 </div><!-- /.col -->
     
-<!-- LES DATA RECRUITERS -->
-<div class="col-wrap style col-gray"><!-- style col-shadow/ col-gray/ bg/ testimonial/-->
+<!-- LES MEMBRES DE L'EQUIPE -->
+<div class="col-wrap"><!-- style col-shadow/ col-gray/ bg/ testimonial/-->
 	<div class="container ">
         <div class="row-fluid">
             <!-- Hadil Filali -->
@@ -54,18 +54,39 @@
                     </div>
                     <div class="team-data">
                         <h2>Hadil Filali</h2>
-                        <p>Founder / COO & Technical AI Data Recruiter</p>
+                        <p>Founder & COO</p>
+                        <p>Technical AI Data Recruiter</p>
                     </div>
                     <div class="team-sr">
                         <ul>
                             <li><a target="_blank" href="mailto:hadil.filali@sparkline.fr"  rel="mail"><span class="icon-mail"></a></li>
                             <li><a target="_blank" href="https://fr.linkedin.com/in/hadilfilali"  rel="linkedin"><span class="icon-linkedin"></a></li>
-                            <li><a target="_blank" href="https://twitter.com/sparklinetalks"  rel="twitter"><span class="icon-twitter"></a></li>
                         </ul>
                         <div class="clear"></div>
                     </div>
                 </div><!-- /team-item -->
-            </div><!-- /.span4 -->		
+            </div><!-- /.span4 -->
+            
+            <!-- David Tonnaire -->
+            <div class="span4">
+                <div class="team services animationBegin">
+                    <div class="team-img">
+                        <img class='themeapt_animated_image themeapt_image themeapt_animate_when_almost_visible right-to-left' src="images/team/david tonnaire.jpg" alt="David Tonnaire">
+                    </div>
+                    <div class="team-data">
+                        <h2>David Tonnaire</h2>
+                        <p>HR Advisor</p>
+                        <p>Recrutement, Culture & Engagement Collaborateur</p>
+                    </div>
+                    <div class="team-sr">
+                        <ul>
+                            <li><a target="_blank" href="https://www.linkedin.com/in/david-tonnairerh/"  rel="linkedin"><span class="icon-linkedin"></a></li>
+                            <li><a target="_blank" href="https://www.welcomeonboard.link/"  rel="home"><span class="icon-home"></a></li>    
+                        </ul>
+                        <div class="clear"></div>
+                    </div>
+                </div><!-- /team-item -->
+            </div><!-- /.span4 -->
             
             <!-- Loïc Knuchel-->
             <div class="span4">
@@ -75,7 +96,8 @@
                     </div>
                     <div class="team-data">
                         <h2>Loïc Knuchel</h2>
-                        <p>Technical Advisor | Software Engineering at scale</p>
+                        <p>Software Engineering Advisor</p>
+                        <p>Architecture & Software Engineering at scale</p>
                     </div>
                     <div class="team-sr">
                         <ul>
@@ -86,15 +108,38 @@
                 </div><!-- /team-item -->
             </div><!-- /.span4 -->
             
+            
+            
         </div><!-- /.row -->			 
     </div><!-- /.container -->
 </div><!-- /.col -->
 
 <!-- DEUXIEME LIGNE -->
-<div class="col-wrap col-grey"><!-- style col-shadow/ col-gray/ bg/ testimonial/-->
+    
+<div class="col-wrap"><!-- style col-shadow/ col-gray/ bg/ testimonial/-->
 	<div class="container ">
-        <div class="row-fluid">           
-            <!-- Prosper -->
+        <div class="row-fluid">
+            <!-- Yasir Khan -->
+            <div class="span4">
+                <div class="team services animationBegin">
+                    <div class="team-img">
+                        <img class='themeapt_animated_image themeapt_image themeapt_animate_when_almost_visible bottom-to-top' src="images/team/Yasir KHAN, PhD Machine Learning Data Science Big Data.jpg" alt="Yasir Khan, Ph.D">
+                    </div>
+                    <div class="team-data">
+                        <h2>Yasir Khan, PhD</h2>
+                        <p>AI Advisor</p>
+                        <p>Consulting AI, Data Science & Machine Learning at scale</p>
+                    </div>
+                    <div class="team-sr">
+                        <ul>
+                            <li><a target="_blank" href="https://www.linkedin.com/in/yasirkhan/"  rel="linkedin"><span class="icon-linkedin"></a></li>
+                        </ul>
+                        <div class="clear"></div>
+                    </div>
+                </div><!-- /team-item -->
+            </div><!-- /.span4 --> 
+            
+            <!-- Prosper Burq-->
             <div class="span4">
                 <div class="team services animationBegin">
                     <div class="team-img">
@@ -102,7 +147,8 @@
                     </div>
                     <div class="team-data">
                         <h2>Prosper Burq</h2>
-                        <p>Technical Advisor | Big Data Architecture & Machine Learning at scale</p>
+                        <p>Big Data Advisor</p>
+                        <p>Acrhitecture Big Data & Machine Learning at scale</p>
                     </div>
                     <div class="team-sr">
                         <ul>
@@ -113,24 +159,6 @@
                 </div><!-- /team-item -->
             </div><!-- /.span4 --> 
             
-            <!-- Yasir KHAN -->
-            <div class="span4">
-                <div class="team services animationBegin">
-                    <div class="team-img">
-                        <img class='themeapt_animated_image themeapt_image themeapt_animate_when_almost_visible bottom-to-top' src="images/team/yasir-khan.jpg" alt="Yasir Khan, Ph.D">
-                    </div>
-                    <div class="team-data">
-                        <h2>Yasir Khan, PhD</h2>
-                        <p>Technical Advisor | AI, Data Science & Machine Learning at scale</p>
-                    </div>
-                    <div class="team-sr">
-                        <ul>
-                            <li><a target="_blank" href="https://www.linkedin.com/in/yasirkhan/"  rel="linkedin"><span class="icon-linkedin"></a></li>
-                        </ul>
-                        <div class="clear"></div>
-                    </div>
-                </div><!-- /team-item -->
-            </div><!-- /.span4 --> 
         </div><!-- /.row -->			 
     </div><!-- /.container -->
 </div><!-- /.col -->
